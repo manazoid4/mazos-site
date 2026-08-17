e197f10651b56613bf63124d273ae643814c4411
manazoid4 <157256328+manazoid4@users.noreply.github.com>
2026-08-17 21:50:47 +0100

    Reduce contact friction for Maz Works enquiries

--- app/site.ts
+++ app/site.ts
@@ -5,7 +5,7 @@ export const CONTACT_EMAIL = 'manazoid4@gmail.com';
 export const GITHUB_URL = 'https://github.com/manazoid4';
 
 export const CONTACT_LINKS = {
-  hire: `mailto:${CONTACT_EMAIL}?subject=Role%20enquiry&body=Hi%20Manazir%2C%0A%0ARole%3A%20%0ACompany%3A%20%0ALocation%2Fremote%3A%20%0A`,
-  client: `mailto:${CONTACT_EMAIL}?subject=Free%20tailored%20demo&body=Hi%20Manazir%2C%0A%0AThe%20problem%20is%3A%20%0AWho%20it%20affects%3A%20%0AWhat%20happens%20today%3A%20%0AWhat%20a%20better%20outcome%20looks%20like%3A%20%0A`,
-  collaborate: `mailto:${CONTACT_EMAIL}?subject=Collaboration&body=Hi%20Manazir%2C%0A%0AWhat%20we%27re%20building%3A%20%0AWhat%20I%27m%20looking%20for%3A%20%0A`,
+  hire: `mailto:${CONTACT_EMAIL}?subject=Role%20enquiry&body=Hi%20Manazir%2C%0A%0AI%27d%20like%20to%20talk%20to%20you%20about%3A%20%0A`,
+  client: `mailto:${CONTACT_EMAIL}?subject=Maz%20Works%20%E2%80%94%20Free%20demo&body=Hi%20Maz%2C%0A%0AI%20want%20help%20with%3A%20%0A%0AWhat%20currently%20happens%3A%20%0A%0AWhat%20I%27d%20like%20instead%3A%20%0A`,
+  collaborate: `mailto:${CONTACT_EMAIL}?subject=Maz%20Works%20collaboration&body=Hi%20Maz%2C%0A%0AI%27d%20like%20to%20talk%20about%3A%20%0A`,
 };
