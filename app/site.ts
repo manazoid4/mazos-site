a5fa8610996ba7036c43ee4ae7367c6b4c75e559
manazoid4 <157256328+manazoid4@users.noreply.github.com>
2026-08-17 22:16:32 +0100

    Improve client enquiry prompt

--- app/site.ts
+++ app/site.ts
@@ -6,6 +6,6 @@ export const GITHUB_URL = 'https://github.com/manazoid4';
 
 export const CONTACT_LINKS = {
   hire: `mailto:${CONTACT_EMAIL}?subject=Role%20enquiry&body=Hi%20Manazir%2C%0A%0AI%27d%20like%20to%20talk%20to%20you%20about%3A%20%0A`,
-  client: `mailto:${CONTACT_EMAIL}?subject=Maz%20Works%20%E2%80%94%20Free%20demo&body=Hi%20Maz%2C%0A%0AI%20want%20help%20with%3A%20%0A%0AWhat%20currently%20happens%3A%20%0A%0AWhat%20I%27d%20like%20instead%3A%20%0A`,
+  client: `mailto:${CONTACT_EMAIL}?subject=Maz%20Works%20%E2%80%94%20Free%20problem%20map%20%2B%20demo&body=Hi%20Maz%2C%0A%0AWhat%20is%20slowing%20us%20down%3A%20%0A%0AWhat%20currently%20happens%3A%20%0A%0AWhat%20we%27d%20like%20instead%3A%20%0A%0AAnything%20we%20already%20use%20that%20you%20should%20know%20about%3A%20%0A`,
   collaborate: `mailto:${CONTACT_EMAIL}?subject=Maz%20Works%20collaboration&body=Hi%20Maz%2C%0A%0AI%27d%20like%20to%20talk%20about%3A%20%0A`,
 };
