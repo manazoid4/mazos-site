bdd86b92b259881d2f3025e20b5ea043d848ad99
manazoid4 <157256328+manazoid4@users.noreply.github.com>
2026-08-23 21:06:44 +0100

    Add Maz Works LinkedIn URL

--- app/site.ts
+++ app/site.ts
@@ -3,6 +3,7 @@ export const SITE_NAME = 'Maz Works';
 export const PERSON_NAME = 'Manazir Hussain';
 export const CONTACT_EMAIL = 'manazoid4@gmail.com';
 export const GITHUB_URL = 'https://github.com/manazoid4';
+export const LINKEDIN_URL = 'https://www.linkedin.com/company/maz-works';
 
 export const CONTACT_LINKS = {
   hire: `mailto:${CONTACT_EMAIL}?subject=Role%20enquiry&body=Hi%20Manazir%2C%0A%0AI%27d%20like%20to%20talk%20to%20you%20about%3A%20%0A`,
